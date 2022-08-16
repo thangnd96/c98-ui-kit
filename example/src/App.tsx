cd4c745502924277242d6daecbc4460f918c331d
ThangND <ndthang996@gmail.com>
2022-08-16 14:11:46 +0700

    init react library

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'ui-kit'
+import 'ui-kit/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="C98 UI KIT 😄" />
+}
+
+export default App
